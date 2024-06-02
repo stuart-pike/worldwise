c4a673c852296a2af8354d4a406afe713d6224b2
stuart-pike <110692951+stuart-pike@users.noreply.github.com>
2024-06-02 09:25:51 -0700

    first commit

---
+++ src/components/PageNav/PageNav.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+
+import Logo from "../Logo/Logo";
+
+function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <li>
+          {/* Include the slash to ensure link begins at the root */}
+          <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
+
+export default AppLayout;
