5f4b08d2c0ec1e25766ee3c32624815e97822327
stuart-pike <110692951+stuart-pike@users.noreply.github.com>
2024-06-18 11:59:47 -0700

    Added getCity to dependency of the useEffect and implemented a useCallack to the getCity function in CitiesContext to prevent infinite loop

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useContext, createContext, useEffect, useReducer } from "react";
 
 const BASE_URL = "http://localhost:9000";
@@ -86,20 +87,23 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: ACTION_TYPES.LOADING });
-    try {
-      const response = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await response.json();
-      dispatch({ type: ACTION_TYPES.CITY_LOADED, payload: data });
-    } catch {
-      dispatch({
-        type: ACTION_TYPES.REJECTED,
-        payload: "Error loading the city...",
-      });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: ACTION_TYPES.LOADING });
+      try {
+        const response = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await response.json();
+        dispatch({ type: ACTION_TYPES.CITY_LOADED, payload: data });
+      } catch {
+        dispatch({
+          type: ACTION_TYPES.REJECTED,
+          payload: "Error loading the city...",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: ACTION_TYPES.LOADING });
